e6bba9089cf4a7a5e697505da05ab1916ed23cbb
Viking Den <vikingden7@gmail.com>
2017-03-31 23:21:27 +0800

    add Example005 Toggle Button

---
+++ src/main/java/com/viking/swt/Example005_ToggleButton.java
@@ -0,0 +1,32 @@
+package com.viking.swt;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Shell;
+
+/**
+ * Author : Viking Den <vikingden7@gmail.com>
+ * Date : 2017/3/31
+ */
+public class Example005_ToggleButton extends BaseExample{
+
+    public static void main(String[] args){
+        Example005_ToggleButton exam005 = new Example005_ToggleButton() ;
+        exam005.createBase();
+    }
+
+
+    protected void createWidget(Shell shell) {
+        shell.setLayout(new FillLayout(SWT.VERTICAL));
+
+        Button toggle1 = new Button(shell , SWT.TOGGLE|SWT.LEFT) ;
+        toggle1.setText("Toggle Left");
+
+        Button toggle2 = new Button(shell , SWT.TOGGLE|SWT.FLAT) ;
+        toggle2.setText("Toggle Flat");
+
+        Button toggle3 = new Button(shell , SWT.TOGGLE|SWT.RIGHT) ;
+        toggle3.setText("Toggle Right");
+    }
+}
